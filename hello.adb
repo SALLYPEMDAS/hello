44d407c5f9a94a830a01b01fe417144cbf99ed00
egilhh <egilhovik@hotmail.com>
2013-06-06 09:50:46 +0200

    doc update

--- hello.adb
+++ hello.adb
@@ -2,5 +2,12 @@ with Ada.Text_IO;
 
 procedure Hello is
 begin
-   Ada.Text_IO.Put_Line("Hello, World of Pi!");
+   Ada.Text_IO.Put_Line
+     ("Hello, World of Pi!");
+   Ada.Text_IO.Put_Line
+     ("Hello, once more");
+   Ada.Text_IO.Put_Line
+     (Item => "Hello, again!");
+   Ada.Text_IO.Put_Line
+     (Item => "Hello from work");
 end Hello;
